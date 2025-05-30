641a464f82aa2a51381a4b5f206a2fd84cab752b
YasserDeveloper89 <yass.gonzaleszea@gmail.com>
2025-05-30 22:15:01 +0200

    Update App.py

--- App.py
+++ App.py
@@ -78,7 +78,7 @@ def generar_word(df_to_export): # Aquí se recibe df_to_export
 
 # ----------------- Panel de Administración -----------------
 def admin_panel():
-    st.title("🛠️ Panel de Administración")
+    st.title("🛠️ Hydromet - Panel de Administración")
     st.write(f"Bienvenido, {st.session_state.usuario}")
 
     st.subheader("📁 Cargar Datos (CSV)")
